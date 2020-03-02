41f29640478dd975c6ebd09cef00d422f86e6bc1
Nikhil Prabhu <i516395@inll34397177b.apj.global.corp.sap>
2020-03-02 14:15:46 +0530

    Add global variables]

--- whitelister.sh
+++ whitelister.sh
@@ -13,4 +13,60 @@
 #   entries into the "webdisptab" and "saprouttab" files. Documentation will be    #
 #   available in the README.md file in project's root directory.                   #
 #                                                                                  #
+#   The script is designed to be as portable and POSIX compliant as possible.      #
+#   The conventions followed in this script can be found at:                       #
+#                                                                                  #
+#                  https://github.com/icy/bash-coding-style                        #
+#                                                                                  #
+#                                                                                  #
 ####################################################################################
+
+####################
+# GLOBAL VARIABLES #
+####################
+
+# Name of the script
+SCRIPT_NAME="$(basename -- $0)"
+
+# Current system time: format - DD/MM/YYYY
+SYS_DATE=$(date +'%d/%m/%Y')
+
+# Current system time: format - H:M:S (24 hour)
+SYS_TIME=$(date +'%H:%M:%S')
+
+# Current process id
+CURRENT_PID=$$
+
+# Lock file
+LOCK_FILE="/tmp/$SCRIPT_NAME.lock"
+
+# Web dispatcher ACL file
+WEB_DISP_TAB="/usr/sap/WD2/whitelist/webdisptab_test"
+
+# Router table file
+SAP_ROUT_TAB="/usr/sap/WD2/whitelist/saprouttab_test"
+
+# Temporary web dispatcher file
+TMP_WEB_DISP_TAB="/tmp/$SCRIPT_NAME-webdisptab.temp"
+
+# Temporary router table file
+TMP_SAP_ROUT_TAB="/tmp/$SCRIPT_NAME-saprouttab.temp"
+
+# Temporary file to store IP addresses in
+TMP_IPS="/tmp/$SCRIPT_NAME-ips.temp"
+
+# Temporary file to store SIDs in
+TMP_SIDS="/tmp/$SCRIPT_NAME-sids.temp"
+
+# Backup of web dispatcher ACL file
+BKP_WEB_DISP_TAB="$WEB_DISP_TAB-$SYS_TIME-$(echo $SYS_DATE | tr '/' '.').backup"
+
+# Backup of router table file
+BKP_SAP_ROUT_TAB="$SAP_ROUT_TAB-$SYS_TIME-$(echo $SYS_DATE | tr '/' '.').backup"
+
+# PID of other running instance (if any)
+if [ -f "$LOCKFILE" ]
+then
+    RUN_PID=$(cat "$LOCKFILE" | tr -d '\n')
+fi
+
