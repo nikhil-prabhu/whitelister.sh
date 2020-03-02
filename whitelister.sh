0f9d54d3c9da418787ec1e065d0b033395802760
Nikhil Prabhu <i516395@inll34397177b.apj.global.corp.sap>
2020-03-02 13:53:00 +0530

    Add documentation banner

---
+++ whitelister.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+####################################################################################
+#                                                                                  #
+#                               WHITELISTER.SH                                     #
+#                               --------------                                     #
+#                                                                                  #
+#   whitelister.sh is a (mostly) POSIX compliant BASH Shell Script that can        #
+#   be used to internally whitelist 3rd party IP addresses by making entries       #
+#   into one or more types of access control list (ACL) files. This version        #
+#   of the script is specifically tailored for use by members of SAP ICC's RAC     #
+#   team, to whitelist the IP addresses of SAP's partners by making appropriate    #
+#   entries into the "webdisptab" and "saprouttab" files. Documentation will be    #
+#   available in the README.md file in project's root directory.                   #
+#                                                                                  #
+####################################################################################
